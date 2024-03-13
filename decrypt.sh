460ba819254616d7ecc0730b6dc36a12cbfb14bd
VIP-VPN <154327305+VIP-VPN@users.noreply.github.com>
2024-03-13 21:34:03 +0800

    Update decrypt.sh

--- decrypt.sh
+++ decrypt.sh
@@ -1,37 +1,38 @@
 #!/bin/sh
 
-export RED='\033[0;31m'
-export GREEN='\033[0;32m'
-export YELLOW='\033[0;33m'
-export BLUE='\033[0;34m'
-export PURPLE='\033[0;35m'
-export CYAN='\033[0;36m'
-export LIGHT='\033[0;37m'
-export NC='\033[0m'
+# Set skip value to 23
+skip=23
 
-PATH="/usr/bin:$PATH"
-x=$(basename $0)
-if test $# = 0; then
-    echo  "${PURPLE}=============================================================${NC}"
-    echo  "                        ${GREEN}MENU DECRYPTER ${NC}"
-    echo  "${CYAN}=============================================================${NC}"
-    echo  "             ${GREEN} Contoh : ketik dec-bzip2 spasi namafile ${NC}"
-    echo  "${YELLOW}-------------------------------------------------------------${NC}"
-    echo  " ${GREEN}   [ Hak Cipta @ -  RMBL VPN  (2024) ] ${NC}            ${RED}#${NC}"
-    echo  "${BLUE}=============================================================${NC}"
-    exit 1
-fi
+# Set umask value
+umask_val=$(umask)
+umask 77
 
-set -C
-tmp=gz$$
-trap "rm -f $tmp; exit 1" HUP INT QUIT TRAP USR1 PIPE TERM
-: > $tmp || exit 1
+# Create temporary file
+tmpfile=$(mktemp -p /tmp gztmp.XXXXXX) || exit 1
 
-bzip2 -d $1 >> $tmp || {
-    rm -f $tmp
-    echo ${x}: dekripsi tidak mungkin untuk $1, file tidak berubah.
-    exit 1
-}
+# Extract the compressed data using bzip2
+if /usr/bin/tail -n +$skip "$0" | /bin/bzip2 -cd >> $tmpfile; then
+  # Reset umask value
+  umask $umask_val
 
-echo "File telah berhasil didekripsi. Hasilnya tersimpan di $tmp."
-exit 0
+  # Set executable permission on the temporary file
+  /bin/chmod 700 $tmpfile
+
+  # Get the name of the original program
+  prog=$(echo $0 | /bin/sed 's|^.*/||')
+
+  # Create a symbolic link to the temporary file
+  if /bin/ln -T $tmpfile "/tmp/$prog" 2>/dev/null; then
+    # Trap to clean up temporary files
+    trap '/bin/rm -f $tmpfile "/tmp/$prog"; exit $res' 0
+    (/bin/sleep 5; /bin/rm -f $tmpfile "/tmp/$prog") 2>/dev/null &
+    /tmp/"$prog" ${1+"$@"}; res=$?
+  else
+    # Trap to clean up temporary files
+    trap '/bin/rm -f $tmpfile; exit $res' 0
+    (/bin/sleep 5; /bin/rm -f $tmpfile) 2>/dev/null &
+    $tmpfile ${1+"$@"}; res=$?
+  fi
+else
+  echo Cannot decompress $0; exit 1
+fi; exit $res
